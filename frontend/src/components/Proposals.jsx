f5f1f5aaa0839bc376dc9baedff988ec37d29330
Aditya <aditya.verma.manit@gmail.com>
2023-05-10 12:57:53 +0000

    add preview

--- frontend/src/components/Proposals.jsx
+++ frontend/src/components/Proposals.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const fetchPreview = async (hash) => {
+    try {
+        const endpoint = `http://cors-proxy.kingsuper.services/?targetApi=https://infura-ipfs.io/ipfs/${hash}`
+        const response = await axios.get(endpoint);
+        const text = response.data;
+        const previewText = text.slice(0, 100); // Change this to adjust the preview length
+        return previewText + '...';
+    } catch (error) {
+        console.error('Error fetching content from IPFS:', error);
+        return 'Error fetching content';
+    }
+};
+
+
 
 const Proposals = ({ posts, address }) => {
+    const [previews, setPreviews] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const fetchedPreviews = await Promise.all(
+                posts.map(async (post) => await fetchPreview(post.content))
+            );
+            setPreviews(fetchedPreviews);
+        })();
+    }, [posts]);
+
     return (
         address && (
             <div>
@@ -10,6 +39,7 @@ const Proposals = ({ posts, address }) => {
                             <h2>
                                 {post.id}. {post.title}
                             </h2>
+                            <p>{previews[index]}</p>
                             <button
                                 className="read-on-ipfs"
                                 onClick={() =>
