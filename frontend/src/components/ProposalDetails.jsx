6822a311a52c64b7e93b2d14ac1bdd3193583172
Aditya <aditya.verma.manit@gmail.com>
2023-05-12 05:28:17 +0000

    display better

--- frontend/src/components/ProposalDetails.jsx
+++ frontend/src/components/ProposalDetails.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './css/ProposalDetails.css'
+import './css/ProposalDetails.css';
+import ReactMarkdown from 'react-markdown';
 
 const ProposalDetails = ({ title, proposalDescription, id }) => {
     return (
         <div className="proposal-page-left">
             <h2>{id}. {title}</h2>
-            <p>{proposalDescription}</p>
+            <hr />
+            <ReactMarkdown children={proposalDescription} />
         </div>
     );
 };
